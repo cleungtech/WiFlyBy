3c54f9157694d203e6178bd26fa450eb78da976d
cleungtech <64727850+cleungtech@users.noreply.github.com>
2023-12-10 02:19:24 -0500

    Initial Commit

---
+++ WiFlyBy/WiFlyByApp.swift
@@ -0,0 +1,17 @@
+//
+//  WiFlyByApp.swift
+//  WiFlyBy
+//
+//  Created by Chi on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct WiFlyByApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
